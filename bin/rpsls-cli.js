0f9e357c4b065c96feafe86c4e5645c2579ae689
jijiezhou <jijiez@email.unc.edu>
2023-11-17 23:54:01 -0500

    rpsls-cli.js logic change

--- bin/rpsls-cli.js
+++ bin/rpsls-cli.js
@@ -1,41 +1,47 @@
-#!/usr/bin / env node
-import { rpls } from '../lib/rpsls.js';
-import minimist from 'minimist';
+#!/usr/bin/env node
+import { rpls } from "../lib/rpsls.js";
+import minimist from "minimist";
 
 const args = minimist(process.argv.slice(2));
 
+//handle with help documentation
 if (args.h || args.help) {
-    printHelp();
-    process.exit(0);
+  printHelp();
+  process.exit(0);
 }
 
+//handle with rules documentation
 if (args.r || args.rules) {
-    printRules();
-    process.exit(0);
+  printRules();
+  process.exit(0);
 }
 
 const res = rps(args._[0]);
 try {
-    console.log(JSON.stringify(res));
+  console.log(JSON.stringify(res));
+  process.exit(0);
 } catch (err) {
-    console.error(`Error: ${err.message}\n`);
+  if (err instanceof RangeError) {
+    console.error(`${res} is out of range`);
     printHelp();
     printRules();
     process.exit(1);
+  }
 }
 
 function printHelp() {
-    console.log(`Usage: node-rpsls [SHOT]\nPlay the Lizard-Spock Expansion of Rock Paper Scissors (RPSLS)!\n\n',
+  console.log(`Usage: node-rpsls [SHOT]\nPlay the Lizard-Spock Expansion of Rock Paper Scissors (RPSLS)!\n\n',
     '  -h, --help        display this help message and exit\n',
     '  -r, --rules       display the rules and exit\n\nExamples:\n',
     '  node-rpsls        Return JSON with single player RPSLS result.\n',
     '                    e.g. {"player":"rock"}\n',
     '  node-rpsls rock   Return JSON with results for RPSLS played against a simulated opponent.\n',
     '                    e.g {"player":"rock","opponent":"Spock","result":"lose"}`);
+  process.exit(0);
 }
 
 function printRules() {
-    console.log(`Rules for the Lizard-Spock Expansion of Rock Paper Scissors:\n\n',
+  console.log(`Rules for the Lizard-Spock Expansion of Rock Paper Scissors:\n\n',
     ' - Scissors CUTS Paper\n',
     ' - Paper COVERS Rock\n',
     ' - Rock SMOOSHES Lizard\n',
@@ -46,4 +52,5 @@ function printRules() {
     ' - Paper DISPROVES Spock\n',
     ' - Spock VAPORIZES Rock\n',
     ' - Rock CRUSHES Scissors`);
+  process.exit(0);
 }
