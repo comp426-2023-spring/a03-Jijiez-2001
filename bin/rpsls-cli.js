76ba9b15aa1b01eb953a41a6eba8d99d6e7674fd
jijiezhou <jijiez@email.unc.edu>
2023-11-18 00:19:27 -0500

    rpsls out of range

--- bin/rpsls-cli.js
+++ bin/rpsls-cli.js
@@ -22,7 +22,7 @@ try {
   process.exit(0);
 } catch (err) {
   if (err instanceof RangeError) {
-    console.error(`${args._[0]} is out of range.`);
+    //console.error(`${args._[0]} is out of range.`);
     printHelp();
     printRules();
     process.exit(1);
